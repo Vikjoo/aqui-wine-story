4e1b1742a545cd4acc2653486cc92553895296c7
rgunoo <rgunoo@192.168.100.2>
2017-09-21 14:33:05 +0400

    update

---
+++ src/main/java/net/jiniguru/roshan/ui/view/customer/ChildInfo.java
@@ -0,0 +1,50 @@
+package net.jiniguru.roshan.ui.view.customer;
+
+import java.util.Optional;
+
+import javax.annotation.PostConstruct;
+
+import org.vaadin.spring.annotation.PrototypeScope;
+
+import com.vaadin.data.BeanValidationBinder;
+import com.vaadin.data.converter.StringToIntegerConverter;
+import com.vaadin.spring.annotation.SpringComponent;
+
+import net.jiniguru.roshan.backend.data.entity.Children;
+import net.jiniguru.roshan.backend.data.entity.OrderItem;
+import net.jiniguru.roshan.backend.data.entity.Product;
+
+@SpringComponent
+@PrototypeScope
+public class ChildInfo extends ChildnfoDesign{
+	private BeanValidationBinder<Children> binder;
+	@PostConstruct
+	public void init() {
+		binder = new BeanValidationBinder<>(Children.class);
+		binder.setRequiredConfigurator(null);
+/*		binder.forField(quantity).withConverter(new StringToIntegerConverter(-1, "Please enter a number"))
+				.bind("quantity");
+*/		binder.bindInstanceFields(this);
+/*		binder.addValueChangeListener(e -> fireProductInfoChanged());
+
+		product.addSelectionListener(e -> {
+			Optional<Product> selectedProduct = e.getFirstSelectedItem();
+			int productPrice = selectedProduct.map(Product::getPrice).orElse(0);
+			updatePrice(productPrice);
+		});
+
+		readOnlyComment.setWidth("100%");
+		readOnlyComment.setId(comment.getId());
+		readOnlyComment.setStyleName(comment.getStyleName());
+
+		delete.addClickListener(e -> fireOrderItemDeleted());*/
+	}
+	@Override
+	public void focus() {
+		name.focus();
+	}
+	public void setItem(Children childItem) {
+		binder.setBean(childItem);
+		
+	}
+}
